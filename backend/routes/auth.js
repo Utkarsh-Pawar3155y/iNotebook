decb06fc76b8c1fabbab0183002b40ea37c5a978
Utkarsh Pawar <pawarutkarsh796@gmail.com>
2026-06-14 21:11:05 +0530

    Add JWT authentication and password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,33 +2,60 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_secret = process.env.JWT_SECRET;
 
 // Create a user and does not require auth
-router.post('/', [
+router.post('/createuser', [
 
     body('name', 'Invalid Name').isLength({ min: 3 }),
     body('password', 'Password length should be greater than 5').isLength({ min: 5 }),
     body('email', 'Invalid Email').isEmail(),
 
 ], async (req, res) => {
+
     const result = validationResult(req);
+
+    //error array catched by express-validator is empty
     if (result.isEmpty()) {
+
+        //bcrypt the password before saving
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
         try {
             const user = new User({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                password: secPass
             });
             await user.save();
+
+            //user id used for signing the token
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            //auth token generated
+            const authToken = jwt.sign(data, JWT_secret);
+
+            return res.json({ authToken });
+
         }
         catch (error) {
             if (error.code === 11000) {
-                return res.send({ error: "Duplicate Email" });
+                return res.status(400).json({
+                    error: "Email already exists"
+                });
             }
-            return res.send({error : "Internal Server error"});
+            return res.status(500).json({
+                error: "Internal Server Error"
+            });
         }
-
-        return res.send(`Hello, ${req.body.name}!`);
     }
 
     res.send({ errors: result.array() });
